81f7abb16248c88f071cc8c5eccd672cb8c02a06
jlampley <jlample5@vols.utk.edu>
2016-04-26 17:35:37 -0400

    This is the Correlation homework - JL

--- CorrelationFunctions.R
+++ CorrelationFunctions.R
@@ -3,23 +3,80 @@ library(geiger) #utilty fns
 library(corHMM)
 library(phylolm)
 library(phytools)
+library(rotl)
 
 VisualizeData <- function(phy, data) {
+  windows()
+  plot (phy)
+  print(str(phy))
+  print(dim(data))
+  windows()
+  barplot(data)
+  print(table(data))
+  windows()
+  hist(data[,1])
+  windows()
+  hist(data[,2])
+  plot(phy) 
+  tiplabels 
 	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
 }
 
 CleanData <- function(phy, data) {
+ 
+Nelumbotree <- treedata(phy, data)
+return (Nelumbotree)
+  
 	#treedata() in Geiger is probably my favorite function in R.
 }
 
 RunContrasts <- function(phy, data, output.pdf="PIC.pdf") {
-	#Include here approaches to save plots, look at your data, regress through the origin, and return the results.
+micpic <- pic(data[,1], phy, scaled=TRUE, var.contrasts=TRUE)
+nicpic <- pic(data[,2], phy, scaled=TRUE, var.contrasts = TRUE)
+  windows()
+  plot(micpic[,1],sqrt(micpic[,2]))
+  windows()
+  plot(nicpic[,1],sqrt(nicpic[,2]))
+  testmic <- cor.test(micpic[,1],sqrt(micpic[,2]))
+  testnic <- cor.test(nicpic[,1],sqrt(nicpic[,2]))
+  
+  pic12 <- cbind(micpic[,1],nicpic[,1]) #positivizing above results
+  pictable2 <- pic12
+  for (i in seq(pic12[,1])){
+    if (pic12[i,1]<0) {
+      pictable2[i,] <- pic12[i,]*(-1)
+    } 
+  }  
+  testposedpicmic <- cor.test(pictable2[,1],pictable2[,2])
+    pdf (output.pdf)
+    plot(pictable2[,1],pictable2[,2], xlab="PICtrait1", ylab="PICtrait2")
+    abline(lm(pictable2[,1]~pictable2[,2]-1)) #-1 to force to the origin
+    dev.off()
+ # PSE? save the variances to standardize the contrasts
+#test whether the stdzation was appropriate - consult the Garland paper
+#no corr. between the standard dev. and the contrasts (take the sqrt of the variances
+ #                                                     then positivize it)
+#then do the corelation of the positivized things
+#then plot(
+ # then do Pagel.
+#)
+  	#Include here approaches to save plots, look at your data, regress through the origin, and return the results.
+return(list(micpic=micpic, nicpic=nicpic, correlationtestm=testmic, 
+            correlationtestn=testnic, testposedpicmic=testposedpicmic))
 }
 
 RunPagel94 <- function(phy, data) {
-	#Calculate the rate estimates under each model, the likelihood of the data under each model, and the model averaged rates. 	Return the results
-}
+  Pagelmeth1 <- fitPagel(phy, data[,1], data[,2], method="ace")
+  Pagelmeth2 <- fitPagel(phy, data[,1], data[,2], method="fitMK")
+  Pagelmeth3 <- fitPagel(phy, data[,1], data[,2], method="fitDiscrete")
+	#Calculate the rate estimates under each model, the likelihood of 
+  #the data under each model, and the model averaged rates. 	Return the results
+  return(list(Pagelmeth1=Pagelmeth1, Pagelmeth2=Pagelmeth2, Pagelmeth3=Pagelmeth3))
+  }
 
-RunOtherMethod <- function(phy, data) {
-	
+Runphylolm <- function(phy, data) {
+	data <- as.data.frame(data)
+  linregrmodel <- phylolm(formula=data[,1]~data[,2], data=data, 
+	 phy=phy, model="BM")
+  return(linregrmodel)
 }
--- RunCorrelations.R
+++ RunCorrelations.R
@@ -1,14 +1,31 @@
-source("CorrelationFunctions.R")
-tree <- read.tree("____PATH_TO_TREE_OR_SOME_OTHER_WAY_OF_GETTING_A_TREE____")
-discrete.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
-continuous.data <- read.csv(file="____PATH_TO_DATA_OR_SOME_OTHER_WAY_OF_GETTING_TRAITS____", stringsAsFactors=FALSE) #death to factors.
+source("C:\\Users\\JAL\\Desktop\\PhyloMeth\\Correlation\\CorrelationFunctions.R")
+phy <- get_study_tree("ot_485", "tree1")
+trait1 <- sim.char (phy,0.3, 1, model="BM")
+trait2 <- sim.char (phy, 0.2, 1, model="speciational")
+q <- matrix(c(-0.3, 0.3, 0.5, -0.5), 2, 2, byrow=TRUE)
+discretetrait1 <- sim.char (phy, q, 1, model="discrete")
+discretetrait2 <- sim.char (phy, q, 1, model="discrete")
 
-cleaned.continuous <- CleanData(tree, continuous.data)
-cleaned.discrete <- CleanData(tree, discrete.data)
-VisualizeData(tree, cleaned.continuous)
-VisualizeData(tree, cleaned.discrete)
-contrasts.answer <- RunContrasts(tree, continuous.data)
+discrete.data <- cbind(discretetrait1, discretetrait2)
+rownames(discrete.data) <- rownames(discretetrait1)
+continuous.data <- cbind(trait1, trait2)
+rownames(continuous.data) <- rownames(trait1)
+
+cleaned.continuous <- CleanData(phy, continuous.data)
+cleaned.discrete <- CleanData(phy, discrete.data)
+tree <- cleaned.discrete$phy
+moss <- cleaned.discrete$data
+moss[moss==2]<-0
+fern <- cleaned.continuous$data
+
+VisualizeData(tree, fern)
+VisualizeData(tree, moss)
+contrasts.answer <- RunContrasts(tree, fern)
 save(list=ls(), file="CorrelationsResults.rda")
-pagel94.answer <- RunPagel(tree, discrete.data)
+pagel94.answer <- RunPagel94(tree, moss)
 save(list=ls(), file="CorrelationsResults.rda")
+
+linregrmodel <- Runphylolm(tree, fern)
+summary(linregrmodel)
+
 
